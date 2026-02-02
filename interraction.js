90c59d86a3e80196e9138c0aa644aff09640e757
Thu Ngo <160973994+Clementine27@users.noreply.github.com>
2026-02-02 09:31:25 -0500

    jss

---
+++ interraction.js
@@ -0,0 +1,23 @@
+// declare variables:
+
+let tama = "awsome"; 
+
+let impala = "sauce"; 
+
+console.log(tama + " " + impala); 
+
+
+let gay; 
+
+gay = 10; 
+
+
+console.log(gay); 
+
+
+// node js is a runtime environment that lets me run js outside of my browser and in the comfort of 
+// my local envi
+
+
+// npm is its pluggin manager 
+
