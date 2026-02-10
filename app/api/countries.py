9536d3e697a6a28cd8966d1853381ac49f37c8cd
Thu Ngo <160973994+Clementine27@users.noreply.github.com>
2026-02-10 17:38:37 -0500

    factory pattern implementation and api setup

--- app/api/countries.py
+++ app/api/countries.py
@@ -1,3 +1,32 @@
 from app.api import bp
+from flask import request
+from app.models import Country
+import sqlalchemy as sa 
+from app import db
 
-# @bp.route()
+
+# all countries by filtering 
+@bp.route("/countries", methods = ["GET"])
+def getCountries(): 
+    query = db.select(Country)
+    
+    continent = request.args.get("continent")
+    # db.get_or_404(Country())
+    if continent != None: 
+        query = query.where(Country.continent == continent)
+    
+    countries = db.session.scalars(query).all()
+    return  [country.to_dict() for country in countries]
+
+# get country by name/name variations 
+@bp.route("/countries/<int:id>", methods = ["GET"])
+def getCountry(id): 
+
+    return db.get_or_404(Country, id).to_dict()
+
+
+# (opt) theme-economic events that happened 
+# during period-(the cold war) that involves  
+# countries-france and vn, where they nations disagreed 
+# on summary-how much does oil cost  
+
---
+++ archive/database.md
@@ -0,0 +1,32 @@
+database: 
+
+1. initialise db instance 
+
+2. define data model for all objects in db 
+
+3. state how the main app should talk to db via model 
+
+
+
+define what you want: 
+- countries 
+- events/year 
+- themes 
+
+define schema 
+
+configure how app should find and communicate with database (app.py setup)
+
+ingests external data 
+
+app accesses data via data models (db model/db schema)
+
+app exposes data via API (communicator btw frontend and backend) so frontend can retrieves 
+
+
+
+
+
+initialise an object that manages data 
+
+then define what are the ground truths that you are interested in (what databases are you holding onto)
