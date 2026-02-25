6430301f266f5bf91e224f62585308e29efe52e5
Thu Ngo <160973994+Clementine27@users.noreply.github.com>
2026-02-24 22:11:52 -0500

    updated schema

--- backend/app/models.py
+++ backend/app/models.py
@@ -4,23 +4,33 @@ from app import db
 import sqlalchemy as sa 
 import sqlalchemy.orm as so 
 import json 
+import datetime as dt 
 
 association_table = sa.Table(
     "association_table",  
     db.Model.metadata, 
     sa.Column(
-        "country_id", sa.ForeignKey("country.id", name = "countryIDKey"), primary_key= True ), 
+        "country_id", sa.ForeignKey("country.id"), primary_key= True), 
     sa.Column(
-        "event_id", sa.ForeignKey("event.id", name = "eventIDKey"), primary_key= True )
+        "event_id", sa.ForeignKey("event.id"), primary_key= True )
 )
 
 themes_table = sa.Table(
     "themes_table", 
     db.Model.metadata, 
     sa.Column(
-        "event_id", sa.ForeignKey("event.id", name = "eventIDKey"), primary_key = True), 
+        "event_id", sa.ForeignKey("event.id"), primary_key = True), 
     sa.Column(
-        "themes_id", sa.ForeignKey("theme.id", name = "themeIDKEY"), primary_key= True)
+        "themes_id", sa.ForeignKey("theme.id"), primary_key= True)
+)
+
+events_table = sa.Table(
+    "events_table", 
+    db.Model.metadata, 
+    sa.Column(
+        "parent_event", sa.ForeignKey("event.id"), primary_key = True), 
+    sa.Column(
+        "child_event", sa.ForeignKey("event.id"), primary_key= True)
 )
 
 class Country(db.Model): 
@@ -45,32 +55,37 @@ class Country(db.Model):
 
         return data 
 
-    
 class Event(db.Model): 
     __tablename__ = "event"
 
     id: so.Mapped[int] = so.mapped_column(primary_key= True)
     name: so.Mapped[str] = so.mapped_column(sa.String(50), nullable = False)
-    startYear: so.Mapped[int] = so.mapped_column(sa.Integer, nullable = False)  
-    endYear: so.Mapped[int] = so.mapped_column(sa.Integer)    
+    start: so.Mapped[dt.date] = so.mapped_column(sa.Date, nullable = False)  
+    end: so.Mapped[dt.date] = so.mapped_column(sa.Date, nullable = False)  
+    
     themes: so.Mapped[list[Theme]] = so.relationship(secondary= themes_table)
     summary: so.Mapped[Optional[str]] = so.mapped_column(sa.Text)
     countries: so.Mapped[list[Country]] = so.relationship(secondary=association_table, back_populates="events")
     
-    childEvents: so.Mapped[list[Event]] = so.relationship("Event", back_populates="parentEvents", remote_side = "event.parentId")
+    # parents: so.Mappeed[list[Event]] = so.relationship(secondary= events_table, back_populates= "children")
+    # children: so.Mappeed[list[Event]] = so.relationship(secondary= events_table, back_populates= "parents")
     
-    parentId: so.Mapped[Optional[int]] = so.mapped_column(sa.ForeignKey("event.id"))
-    
-    parentEvents: so.Mapped[list[Event]] = so.relationship("Event", back_populates= "childEvents", remote_side= "event.id")
-        
+    children:  so.Mapped[list[Event]] = so.relationship('Event', 
+                                                         secondary = (events_table.c.child_event == id), 
+                                                         primaryjoin = (events_table.c.parent_event == id), 
+    secondaryjoin = (Event.c.followed_id == id), 
+    backref = db.backref('events_table', lazy = 'dynamic'), 
+    lazy = 'dynamic')
+
+
     def __repr__(self):
         return '<Events {}>'.format(self.name)
     
     def to_dict(self): 
         data = {
             "name": self.name, 
-            "startYear": self.startYear, 
-            "endYear": self.endYear
+            "start": self.start, 
+            "end": self.end
         } 
         if self.countries: 
             data["countryIDs"] = [country.id for country in self.countries]
@@ -80,11 +95,11 @@ class Event(db.Model):
         if self.themes: 
             data["themes"] = [theme.name for theme in self.themes]      
         
-        if self.childEvents: 
-            data["childEventIDs"] = [event.id for event in self.childEvents]
+        # if self.childEvents: 
+        #     data["childEventIDs"] = [event.id for event in self.childEvents]
         
-        if self.parentEvents: 
-            data["parentEventIDs"] = [event.id for event in self.parentEvents]
+        # if self.parentEvents: 
+        #     data["parentEventIDs"] = [event.id for event in self.parentEvents]
        
         return data
 
